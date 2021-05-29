7adbc28502f0a2a80854fb85df11c3e4315c92eb
Minh <tranhoangminh1707@gmail.com>
2021-05-29 13:52:01 +0900

    Adding login, register function with error handling

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,25 @@
-import { ChakraProvider, ColorModeProvider } from '@chakra-ui/react'
+import { CSSReset, ThemeProvider } from "@chakra-ui/react";
+import React from "react";
+import { Provider, createClient } from "urql";
 
-import theme from '../theme'
+import theme from "../theme";
 
-function MyApp({ Component, pageProps }) {
-  return (
-    <ChakraProvider resetCSS theme={theme}>
-      <ColorModeProvider
-        options={{
-          useSystemColorMode: true,
-        }}
-      >
-        <Component {...pageProps} />
-      </ColorModeProvider>
-    </ChakraProvider>
-  )
+function MyApp({ Component, pageProps }: any) {
+	const client = createClient({
+		url: "http://localhost:4000/graphql",
+		fetchOptions: {
+			credentials: "include", // Send cookie with requests
+		},
+	});
+
+	return (
+		<Provider value={client}>
+			<ThemeProvider theme={theme}>
+				<CSSReset />
+				<Component {...pageProps} />
+			</ThemeProvider>
+		</Provider>
+	);
 }
 
-export default MyApp
+export default MyApp;
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -3,26 +3,26 @@ import { Formik, Form } from "formik";
 import { Box, Button } from "@chakra-ui/react";
 import { Wrapper } from "../components/Wrapper";
 import { InputField } from "../components/InputField";
-import { useRegisterMutation } from "../generated/graphql";
 import { toErrorMap } from "../utils/toErrorMap";
 import { useRouter } from "next/router";
+import { useLoginMutation } from "../generated/graphql";
 
-interface registerProps {}
+interface loginProps {}
 
-const Register: React.FC<registerProps> = ({}) => {
+const Login: React.FC<loginProps> = ({}) => {
 	const router = useRouter();
-	const [, register] = useRegisterMutation();
+	const [, login] = useLoginMutation();
+
 	return (
 		<Wrapper variant="small">
 			<Formik
 				initialValues={{ username: "", password: "" }}
 				onSubmit={async (values, { setErrors }) => {
-					const response = await register(values);
-					console.log(response.data);
+					const response = await login({ options: values });
 
-					if (response.data?.register.errors) {
-						setErrors(toErrorMap(response.data.register.errors));
-					} else if (response.data?.register.user) {
+					if (response.data?.login.errors) {
+						setErrors(toErrorMap(response.data.login.errors));
+					} else if (response.data?.login.user) {
 						// worked
 						router.push("/");
 					}
@@ -49,7 +49,7 @@ const Register: React.FC<registerProps> = ({}) => {
 							isLoading={isSubmitting}
 							colorScheme="teal"
 						>
-							register
+							Login
 						</Button>
 					</Form>
 				)}
@@ -58,4 +58,4 @@ const Register: React.FC<registerProps> = ({}) => {
 	);
 };
 
-export default Register;
+export default Login;
